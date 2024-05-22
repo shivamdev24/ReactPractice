64a342db22ef9e51cf6f5d7bb4f936809920dc5e
Shubham-1213 <shubhamgujarathi14@gmail.com>
2024-05-22 23:45:24 +0530

    Added Medical Consumable page and fixed the routing issue

---
+++ src/pages/productPage/medicalConsumablesPage/BloodCollection.jsx
@@ -0,0 +1,63 @@
+const images = import.meta.glob("../../../assets/img/MediConsumables/BloodCollection/*.jpg", {
+    eager: true,
+  });
+  
+  const descriptions = [
+    "Serum Test Tubes",
+    "Serum Seperating Tubes",
+    "EDTA Blood Draw Tubes",
+    "Sodium Citrate Tubes",
+    "Lithium Heparin Tubes",
+    "ESR Vacuum Blood Tubes",
+    "Multi Sample Blood Collection Needles",
+    "Fluoride Oxalate Tubes",
+    "ESR Rack", 
+    "Safety Lancets Twist Off",
+    "Tourniquet"
+  ];
+  const sortedImages = Object.keys(images)
+    .sort((a, b) => parseInt(a.match(/\d+/)[0]) - parseInt(b.match(/\d+/)[0]))
+    .map((key) => images[key].default);
+  
+  const imageArray = sortedImages.map((image, index) => ({
+    image: image,
+    description: descriptions[index],
+  }));
+  
+  function BloodCollection() {
+    return (
+      <div>
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 w-full max-w-7xl mx-auto">
+            {imageArray.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col border border-gray-400 rounded hover:shadow-lg duration-500 items-center"
+              >
+                <div className=" rounded p-2">
+                  <div className="w-full sm:w-48 md:w-60 lg:w-64 h-64 flex items-center justify-center">
+                    <img
+                      src={item.image}
+                      alt={`Image ${index + 1}`}
+                      className="w-full h-full rounded object-contain"
+                      style={{
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                        objectFit: "contain",
+                      }}
+                    />
+                  </div>
+                </div>
+                <p className="text-center m-2 text-black text-sm md:text-lg">
+                  {item.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default BloodCollection;
+  
---
+++ src/pages/productPage/medicalConsumablesPage/MCOutlet.jsx
@@ -0,0 +1,41 @@
+import { Outlet } from "react-router-dom"
+import MCSidebar from "./MCSidebar";
+
+import AboutImg from "../../../assets/img/AboutUs/AboutUs.jpg";
+
+
+export default function MCOutlet() {
+  return (
+    <div className="">
+      <div className="relative">
+        <div className="overlay h-[29rem] opacity-[.6] bg-graydark absolute w-full "></div>
+        <img
+          src={AboutImg}
+          className="h-[29rem] object-top w-full object-cover  "
+          alt=""
+        />
+        <div className="absolute top-48 left-[30px] md:left-[11rem]">
+          <div className="flex gap-4 items-center font-bold text-white">
+            <span>Home</span>
+            <hr className="h-4 w-[2px] bg-white" />
+            <span>Medical Consumables</span>
+          </div>
+          <div className="mt-4">
+            <span className="text-6xl font-bold text-white">MEDICAL CONSUMABLES</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-around my-5">
+        <div className="w-[80%] flex flex-col lg:flex-row justify-around">
+          <div className="">
+            <MCSidebar />
+          </div>
+          <div className=" px-5">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
